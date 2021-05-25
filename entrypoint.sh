8fa40a7e0243420b6429ce35ca6a207a7705f9ed
rkathir-solink <rkathir@solinkcorp.com>
2021-05-25 15:06:18 -0400

    swap sync with cp command

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,11 +22,6 @@ if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-1"
 fi
 
-# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
-if [ -n "$AWS_S3_ENDPOINT" ]; then
-  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
-fi
-
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
@@ -39,10 +34,7 @@ EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
-              --no-progress \
-              ${ENDPOINT_APPEND} $*"
+sh -c "aws s3 cp ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR}"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
